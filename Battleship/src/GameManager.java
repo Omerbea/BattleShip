f3eade2ebb083edf141a6eee4765a09dc883685e
Omer Ben Amram <omerbea@gmail.com>
2017-08-09 17:03:25 +0300

    l

--- Battleship/src/GameManager.java
+++ Battleship/src/GameManager.java
@@ -2,16 +2,20 @@ import GameParser.BattleShipGame;
 
 public class GameManager {
 
-    public static void main(String [] args) {
-        Parser p = new Parser();
-        BattleShipGame b = p.GetParsedGame();
-        Factory f = new Factory();
-        try {
-            f.CreatePlayers();
-        } catch (Exception e) {
-            System.out.printf(e.getMessage());
-        }
+    public static void main(String [] args) throws Exception {
+//        Parser p = new Parser();
+//        BattleShipGame b = p.GetParsedGame();
+//        Factory f = new Factory();
+//        try {
+//            f.CreatePlayers();
+//        } catch (Exception e) {
+//            System.out.printf(e.getMessage());
+//        }
+//
+//        System.out.println();
 
-        System.out.println();
+        Validator v = new Validator();
+        v.ValidateUserMove("     A111 222   333                 4");
+
     }
 }
--- Battleship/src/Validator.java
+++ Battleship/src/Validator.java
@@ -5,6 +5,7 @@ import GameParser.BattleShipGame.Boards.Board.Ship;
 import GameParser.BattleShipGame.ShipTypes.ShipType;
 
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 
 public class Validator {
@@ -17,9 +18,9 @@ public class Validator {
         }
     }
 
-    public void ValidateGameBoards(List<Board> GameBoards) throws Exception {
+    public void ValidateGameBoards(List<Board> gameBoards) throws Exception {
         // Ship type validation
-        for(Board GameBoard : GameBoards) {
+        for(Board GameBoard : gameBoards) {
             for(Ship ship : GameBoard.getShip()) {
                 if(!isShipTypeSupported(ship.getShipTypeId())) {
                     // Ship type not supported
@@ -36,5 +37,23 @@ public class Validator {
             return true ;
         }
         return false ;
+    }
+
+    public void ValidateUserMove(String move) throws Exception {
+
+        String []splittedMove = move.split("[ ]+");
+        int r , c ;
+        // check if first element is ""
+        if(splittedMove[0].equals("")) {
+            try {
+                r = Integer.parseInt(splittedMove[1]);
+                c = Integer.parseInt(splittedMove[2]);
+                System.out.printf("numbers are %d %d" , r ,c );
+
+            } catch (Exception e) {
+
+                System.out.println(e.getMessage() + "that whay");
+            }
+        }
     }
 }
