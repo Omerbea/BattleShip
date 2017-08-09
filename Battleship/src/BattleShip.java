e6ed47c08d758f8088d1826008503a44389e53bf
Jonathan Benedek <jonathan.benedek@mce-sys.com>
2017-08-09 13:51:21 +0300

    start work on Player class

--- Battleship/src/BattleShip.java
+++ Battleship/src/BattleShip.java
@@ -1,2 +1,23 @@
-public class BattleShip extends GameTool {
+final public class BattleShip extends GameTool {
+
+    private String type;
+    private int size;
+    public BattleShip (String i_species, String i_type, int i_size){
+        super(i_species);
+        type = i_type;
+        size = i_size;
+    }
+
+    /*return true if the ship distroyed*/
+    public boolean updateHitMe() {
+        if (size == 0 ){
+            System.out.print("ERROR: BattleShip.updateHitMe() -  size = 0 ");
+            //TODO: THROW EXCEPTION
+        }
+        size--;
+        if (size == 0){
+            return true;
+        }
+        return false;
+    }
 }
--- Battleship/src/Mine.java
+++ Battleship/src/Mine.java
@@ -1,2 +1,4 @@
 public class Mine extends GameTool {
+
+
 }
