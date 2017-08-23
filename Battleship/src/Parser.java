37105741efc380510a96a474c4be8b71f6b815e5
Omer Ben Amram <omerbea@gmail.com>
2017-08-23 13:39:53 +0300

    Validating file added

--- Battleship/src/Parser.java
+++ Battleship/src/Parser.java
@@ -1,9 +1,11 @@
 import GameParser.BattleShipGame;
 
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import java.nio.file.spi.FileTypeDetector;
 
 public class Parser {
 
@@ -22,15 +24,37 @@ public class Parser {
     private void ParseXML() throws Exception {
         try {
 
-            File file = new File("resources\\battleShip_5_basic.xml");
-            JAXBContext jaxbContext = JAXBContext.newInstance(BattleShipGame.class);
+            String xmlPath = "resources\\battleShip_5_basic.xml";
 
-            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-            ParsedGame = (BattleShipGame) jaxbUnmarshaller.unmarshal(file);
+            File file = new File(xmlPath);
+
+            if(file.exists() && !file.isDirectory()) {
+
+                int dotIndex = file.getName().lastIndexOf('.');
+                if(dotIndex == -1) {
+                    throw new Exception("File extension is not recognize.");
+                } else {
+                    if(!file.getName().substring(dotIndex + 1).equals("xml")) {
+                        throw new Exception("File must be of format xml .");
+                    }
+                }
+                JAXBContext jaxbContext = JAXBContext.newInstance(BattleShipGame.class);
+
+                Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+                ParsedGame = (BattleShipGame) jaxbUnmarshaller.unmarshal(file);
+            } else {
+                throw new Exception("File " + xmlPath + " is not exsist ." );
+            }
 
 
         } catch (JAXBException e) {
             throw new Exception("File Cannot be Parsed by JAXB");
+        }
+    }
+
+    public String getFileExtension(String fullName) throws Exception {
+        if(fullName == null); {
+            throw new Exception("Path is null");
         }
     }
 }
